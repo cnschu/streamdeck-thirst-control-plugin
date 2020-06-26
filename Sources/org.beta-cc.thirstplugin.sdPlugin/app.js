826428ee04f4dd8014f0ad58c3a7d1dea20ed730
cnschu <christian.schulz.nuernberg@gmail.com>
2020-06-26 17:37:14 +0200

    streamlined

--- Sources/org.beta-cc.thirstplugin.sdPlugin/app.js
+++ Sources/org.beta-cc.thirstplugin.sdPlugin/app.js
@@ -8,13 +8,12 @@ function connected(jsn) {
 	/** subscribe to the willAppear and other events */
     $SD.on('org.beta-cc.thirstplugin.sendmessage.willAppear', jsonObj => sendMessage.onWillAppear(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.sendmessage.keyUp', jsonObj => sendMessage.onKeyUp(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.sendmessage.sendToPlugin', jsonObj => sendMessage.onSendToPlugin(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.sendmessage.didReceiveSettings', jsonObj => sendMessage.onDidReceiveSettings(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.sendmessage.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
+        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] sendmessage.propertyInspectorDidAppear:');
     });
     $SD.on('org.beta-cc.thirstplugin.sendmessage.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
+        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] sendmessage.propertyInspectorDidDisappear:');
     });
 };
 
@@ -35,94 +34,28 @@ var sendMessage = {
 
     onKeyUp: function (jsn) {
         console.log("[app.js] sendMessage.keyUp: ", jsn.payload.settings);
-		this.sendDiscordMessage(jsn,this.settings[jsn.context].webhook,this.settings[jsn.context].message);
+		sendDiscordMessage(jsn,this.settings[jsn.context].webhook,this.settings[jsn.context].message);
     },
-
-    onSendToPlugin: function (jsn) {
-        /**
-         * this is a message sent directly from the Property Inspector 
-         * (e.g. some value, which is not saved to settings) 
-         * You can send this event from Property Inspector (see there for an example)
-         */ 
-
-        const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
-        if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-            this.doSomeThing({ [sdpi_collection.key] : sdpi_collection.value }, 'onSendToPlugin', 'fuchsia');            
-        }
-    },
-
-    /**
-     * This snippet shows, how you could save settings persistantly to Stream Deck software
-     * It is not used in this example plugin.
-     */
-
-    saveSettings: function (jsn, sdpi_collection) {
-        console.log('saveSettings:', jsn);
-        if (sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
-            if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-                this.settings[sdpi_collection.key] = sdpi_collection.value;
-                console.log('setSettings....', this.settings);
-                $SD.api.setSettings(jsn.context, this.settings);
-            }
-        }
-    },
-
-    /**
-     * Here's a quick demo-wrapper to show how you could change a key's title based on what you
-     * stored in settings.
-     * If you enter something into Property Inspector's name field (in this demo),
-     * it will get the title of your key.
-     * 
-     * @param {JSON} jsn // the JSON object passed from Stream Deck to the plugin, which contains the plugin's context
-     * 
-     */
-
-    setTitle: function(jsn) {
-        if (this.settings && this.settings.hasOwnProperty('mynameinput')) {
-            console.log("watch the key on your StreamDeck - it got a new title...", this.settings.mynameinput);
-            $SD.api.setTitle(jsn.context, this.settings.mynameinput);
-        }
-    },
-
-    /**
-     * Finally here's a methood which gets called from various events above.
-     * This is just an idea how you can act on receiving some interesting message
-     * from Stream Deck.
-     */
-
-    doSomeThing: function(inJsonData, caller, tagColor) {
-        console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
-        // console.log(inJsonData);
-    }, 
-	
-	/**
-	* send a Message to discord via the webhook
-	*/
-	sendDiscordMessage: function(jsn, webhook, message) {
-        console.log("[app.js] sendMessage.sendDiscordMessage: ", jsn.payload.settings, webhook, message);
-
-		var payload = { "content": message };
-		
-		$.ajaxSetup({
-          contentType: "application/json; charset=utf-8",
-          dataType: "json",
-          success: function(data) {
-            console.log('[app.js] Sending event: showOk');
-            $SD.api.showOk(jsn.context);
-          },
-          failure: function(data) {
-            console.log('[app.js] Sending event: showAlert');
-            $SD.api.showAlert(jsn.context);
-          },
-          error: function(data) {
-            console.log('[app.js] Sending event: showAlert');
-            $SD.api.showAlert(jsn.context);
-          },
-        });
-		
-		// Post to the Webhook
-        $.post(webhook, JSON.stringify(payload));
-	},
-
 };
 
+function sendDiscordMessage(jsn, webhook,message){
+	console.log("[app.js] sendDiscordMessage(): ", jsn, webhook, message);
+	var payload = { "content": message };
+	$.ajaxSetup({
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function(data) {
+			console.log('[app.js] Sending event: showOk');
+			$SD.api.showOk(jsn.context);
+		},
+		failure: function(data) {
+			console.log('[app.js] Sending event: showAlert');
+			$SD.api.showAlert(jsn.context);
+		},
+		error: function(data) {
+			console.log('[app.js] Sending event: showAlert');
+			$SD.api.showAlert(jsn.context);
+		},
+	});
+	$.post(webhook, JSON.stringify(payload));
+};

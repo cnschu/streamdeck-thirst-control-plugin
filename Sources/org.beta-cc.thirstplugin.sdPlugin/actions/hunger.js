2d2178ffca3b229a73ee4f358662375813b459e8
cnschu <christian.schulz.nuernberg@gmail.com>
2020-07-05 23:12:58 +0200

    code cleanup

--- Sources/org.beta-cc.thirstplugin.sdPlugin/actions/hunger.js
+++ Sources/org.beta-cc.thirstplugin.sdPlugin/actions/hunger.js
@@ -2,43 +2,15 @@
 ** all hunger related functions who are not part of the vampire-object
 */
 function connect_hunger () {
-    $SD.on('org.beta-cc.thirstplugin.sethunger.willAppear', jsonObj => setHunger.onWillAppear(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.sethunger.keyUp', jsonObj => setHunger.onKeyUp(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.sethunger.didReceiveSettings', jsonObj => setHunger.onDidReceiveSettings(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.sethunger.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] sethunger.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.sethunger.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] sethunger.propertyInspectorDidDisappear:');
-    });
+	$SD.on('org.beta-cc.thirstplugin.sethunger.willAppear', jsonObj => setHunger.onWillAppear(jsonObj));
+	$SD.on('org.beta-cc.thirstplugin.sethunger.keyUp', jsonObj => setHunger.onKeyUp(jsonObj));
+	$SD.on('org.beta-cc.thirstplugin.sethunger.didReceiveSettings', jsonObj => setHunger.onDidReceiveSettings(jsonObj));
 
-    $SD.on('org.beta-cc.thirstplugin.inchunger.willAppear', jsonObj => incHunger.onWillAppear(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.inchunger.keyUp', jsonObj => incHunger.onKeyUp(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.inchunger.didReceiveSettings', jsonObj => incHunger.onDidReceiveSettings(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.inchunger.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] inchunger.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.inchunger.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] inchunger.propertyInspectorDidDisappear:');
-    });
+	$SD.on('org.beta-cc.thirstplugin.inchunger.keyUp', jsonObj => incHunger.onKeyUp(jsonObj));
 
-    $SD.on('org.beta-cc.thirstplugin.dechunger.willAppear', jsonObj => decHunger.onWillAppear(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.dechunger.keyUp', jsonObj => decHunger.onKeyUp(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.dechunger.didReceiveSettings', jsonObj => decHunger.onDidReceiveSettings(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.dechunger.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] dechunger.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.dechunger.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] dechunger.propertyInspectorDidDisappear:');
-    });
+	$SD.on('org.beta-cc.thirstplugin.dechunger.keyUp', jsonObj => decHunger.onKeyUp(jsonObj));
 
-    $SD.on('org.beta-cc.thirstplugin.disphunger.willAppear', jsonObj => dispHunger.onWillAppear(jsonObj));
-	$SD.on('org.beta-cc.thirstplugin.disphunger.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] dechunger.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.disphunger.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] dechunger.propertyInspectorDidDisappear:');
-    });
+	$SD.on('org.beta-cc.thirstplugin.disphunger.willAppear', jsonObj => dispHunger.onWillAppear(jsonObj));
 };
 
 var setHunger = {
@@ -59,15 +31,6 @@ var setHunger = {
 };
 
 var incHunger = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        this.settings[jsn.context] = Utils.getProp(jsn, 'payload.settings', {});
-    },
-
-    onWillAppear: function (jsn) {
-        this.settings[jsn.context] = jsn.payload.settings;
-	},
-
     onKeyUp: function (jsn) {
 		vampire.hunger = vampire.hunger + 1;
 		if (vampire.hunger>5) {vampire.hunger = 5;};
@@ -76,15 +39,6 @@ var incHunger = {
 };
 
 var decHunger = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        this.settings[jsn.context] = Utils.getProp(jsn, 'payload.settings', {});
-    },
-
-    onWillAppear: function (jsn) {
-        this.settings[jsn.context] = jsn.payload.settings;
-	},
-
     onKeyUp: function (jsn) {
 		vampire.hunger = vampire.hunger - 1;
 		if (vampire.hunger<0) {vampire.hunger = 0;};
@@ -93,10 +47,6 @@ var decHunger = {
 };
 
 var dispHunger = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        this.settings[jsn.context] = Utils.getProp(jsn, 'payload.settings', {});
-    },
     onWillAppear: function (jsn) {
         // register the context for refresh
 		vampire.addhdnc(jsn.context);

2dee9d161481ddf2ad7dfe57d8d6359a3d872b53
cnschu <christian.schulz.nuernberg@gmail.com>
2020-07-05 23:20:28 +0200

    code cleanup

--- Sources/org.beta-cc.thirstplugin.sdPlugin/actions/bp.js
+++ Sources/org.beta-cc.thirstplugin.sdPlugin/actions/bp.js
@@ -5,40 +5,12 @@ function connect_bp () {
     $SD.on('org.beta-cc.thirstplugin.setbp.willAppear', jsonObj => setBP.onWillAppear(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.setbp.keyUp', jsonObj => setBP.onKeyUp(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.setbp.didReceiveSettings', jsonObj => setBP.onDidReceiveSettings(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.setbp.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] setbp.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.setbp.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] setbp.propertyInspectorDidDisappear:');
-    });
 
-    $SD.on('org.beta-cc.thirstplugin.incbp.willAppear', jsonObj => incBP.onWillAppear(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.incbp.keyUp', jsonObj => incBP.onKeyUp(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.incbp.didReceiveSettings', jsonObj => incBP.onDidReceiveSettings(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.incbp.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] incbp.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.incbp.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] incbp.propertyInspectorDidDisappear:');
-    });
 
-    $SD.on('org.beta-cc.thirstplugin.decbp.willAppear', jsonObj => decBP.onWillAppear(jsonObj));
     $SD.on('org.beta-cc.thirstplugin.decbp.keyUp', jsonObj => decBP.onKeyUp(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.decbp.didReceiveSettings', jsonObj => decBP.onDidReceiveSettings(jsonObj));
-    $SD.on('org.beta-cc.thirstplugin.decbp.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] decbp.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.decbp.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] decbp.propertyInspectorDidDisappear:');
-    });
 
     $SD.on('org.beta-cc.thirstplugin.dispbp.willAppear', jsonObj => dispBP.onWillAppear(jsonObj));
-	$SD.on('org.beta-cc.thirstplugin.dispbp.propertyInspectorDidAppear', jsonObj => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js] decbp.propertyInspectorDidAppear:');
-    });
-    $SD.on('org.beta-cc.thirstplugin.dispbp.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js] decbp.propertyInspectorDidDisappear:');
-    });
 };
 
 var setBP = {
@@ -60,15 +32,6 @@ var setBP = {
 };
 
 var incBP = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        this.settings[jsn.context] = Utils.getProp(jsn, 'payload.settings', {});
-    },
-
-    onWillAppear: function (jsn) {
-        this.settings[jsn.context] = jsn.payload.settings;
-	},
-
     onKeyUp: function (jsn) {
 		vampire.bp = vampire.bp + 1;
 		if (vampire.bp>8) {vampire.bp = 8;};
@@ -77,15 +40,6 @@ var incBP = {
 };
 
 var decBP = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        this.settings[jsn.context] = Utils.getProp(jsn, 'payload.settings', {});
-    },
-
-    onWillAppear: function (jsn) {
-        this.settings[jsn.context] = jsn.payload.settings;
-	},
-
     onKeyUp: function (jsn) {
 		vampire.bp = vampire.bp - 1;
 		if (vampire.bp<0) {vampire.bp = 0;};
@@ -94,10 +48,6 @@ var decBP = {
 };
 
 var dispBP = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        this.settings[jsn.context] = Utils.getProp(jsn, 'payload.settings', {});
-    },
     onWillAppear: function (jsn) {
         console.log("[app.js] dispBP.onWillAppear invoked", this.bpdnc);
 		// register the context for refresh
